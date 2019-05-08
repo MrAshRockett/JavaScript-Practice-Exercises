5c05fd9f205e42bddf46ab76dad9cccc37b022aa
Richard Hpa <richard.hpa@hotmail.com>
2019-05-09 11:42:06 +1200

    completed task 1

--- js/task01.js
+++ js/task01.js
@@ -1,1 +1,16 @@
 // Create an script which checks to see if someone is old enough to by alcohol
+checkAge();
+
+function checkAge(){
+    var age = parseInt(prompt("How old are you?"));
+    if(age){
+        if(age >= 18){
+            console.log("You can buy alcohol");
+        } else {
+            console.log("You cannot buy alcohol");
+        }
+    } else {
+        console.log("need to start again");
+        checkAge();
+    }
+}
