317d4c06786825fab339e95b1ed70a5af69b78c4
Richard Hpa <richard.hpa@hotmail.com>
2019-05-15 11:52:06 +1200

    completed task 5

--- js/task05.js
+++ js/task05.js
@@ -1,1 +1,10 @@
 // Create an odd and even number checker
+
+function checkOddEven(){
+    var numberToCheck = parseInt(prompt("Please enter a number"));
+    if( (numberToCheck % 2) == 0){
+        console.log(numberToCheck + " is an even number");
+    } else {
+        console.log(numberToCheck + " is an odd number");
+    }
+}
