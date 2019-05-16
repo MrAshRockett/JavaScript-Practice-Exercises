3720216a9d3148c859bc29aee5cdf3f27d1685a7
Richard Hpa <richard.hpa@hotmail.com>
2019-05-17 09:59:02 +1200

    added js tasks 6 and 7

--- js/task06.js
+++ js/task06.js
@@ -1,1 +1,10 @@
 // Create an script that lists everyone in the class from a prompt, 1 prompt per name
+var numberOfStudents = parseInt(prompt("How many students are there in the class"));
+console.log(numberOfStudents);
+
+var list = document.getElementById('studentsNames');
+
+for (var i = 0; i < numberOfStudents; i++) {
+    var name = prompt('what is student #'+(i+1)+' name');
+    list.innerHTML += '<li>'+name+'</li>';
+}
--- js/task07.js
+++ js/task07.js
@@ -1,2 +1,20 @@
 // Create an script that calculates the average number from an array
 // 7a. Numbers then can be added from prompt and new average given
+
+var numbers = [34, 54, 23, 64, 29 ,42 ,68, 35 ];
+var total = 0;
+for (var i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+}
+console.log(total);
+
+var average = total / numbers.length;
+console.log(average);
+
+function getNewAverage(){
+    var newNumber = parseInt(prompt("what is the new number"));
+    numbers.push(newNumber);
+    total = total + newNumber;
+    var newAverage = total / numbers.length;
+    console.log(newAverage);
+}
