98fc4dc386af7ac6faea5405840d6cf768a668b8
Ash Rockett <ashrockett19@gmail.com>
2022-10-12 17:07:40 +1300

    initial commit on new Branch

--- js/task01.js
+++ js/task01.js
@@ -1,2 +1,11 @@
 // Create an script which ask you your age to see if someone is old enough to by alcohol
 
+function ageAppropriateDrinks(age) {
+  if (age < 18) {
+    return prompt('You are not old enough for this product.');
+  } else {
+    return prompt('Enjoy your drink.');
+  }
+}
+
+ageAppropriateDrinks()
