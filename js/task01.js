b73770552159028ac567dabae60df23094000870
Ash Rockett <ashrockett19@gmail.com>
2022-10-13 11:38:13 +1300

    Task 2 completed. Commented out console.log on task 1. Tidy up.

--- js/task01.js
+++ js/task01.js
@@ -4,10 +4,10 @@ let age = parseInt(prompt('What is your age?'))
 function ageAppropriateDrinks() {
 
   if (age >= 18) {
-      console.log('Old enough')
+      // console.log('Old enough')
       return alert('Enjoy your drink.')
   } else {
-      console.log('Not old enough')
+      // console.log('Not old enough')
       return alert('You are not old enough for this product.')
   }
 }
